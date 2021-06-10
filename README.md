2f9447fe2ec2049e1d8974241e1149ca6274d394
GammaWhiskey <speedmgaming@gmail.com>
2021-06-09 23:49:27 -0400

    First version of Discord-to-Roblox group shout system

---
+++ README.md
@@ -0,0 +1,9 @@
+# RAJ Bot
+
+## Setup Instructions
+1. Have Python 3.9 installed
+2. Clone repo
+3. Install roblox-py FROM SOURCE using `pip install git+https://github.com/KILR007/roblox.py`
+4. Install `discord.py` as usual
+5. Fill in relevant details in `settings_template.json` and rename it to `settings.json`
+6. Run `rajbot.py`
---
+++ rajbot.py
@@ -0,0 +1,74 @@
+import json
+import re
+import asyncio
+import time
+
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+from roblox_py import Client as RobloxClient
+
+import discord
+
+settings_file = open("settings.json")
+settings = json.load(settings_file)
+credentials = settings["credentials"]
+
+roblox_client = RobloxClient(cookies=credentials["roblox_token"])
+
+async def format_and_shout(client, message):
+    # Remove all instances of @everyone / @here, role mentions, and user mentions
+    formatted_shout_message = re.sub(r"(@\w+\W?)|(<@(!|&)\w+>\W?)", "", message.content)
+
+    username_to_use = message.author.nick
+    if not username_to_use:
+        username_to_use = message.author.name
+
+    formatted_shout_message = username_to_use + ": " + formatted_shout_message
+
+    try:
+        performance_timer_start = time.perf_counter()
+        auth_group = await roblox_client.get_auth_group(settings["roblox_group_id"])
+        await auth_group.change_shout(formatted_shout_message)
+    except:
+        embed_var = discord.Embed(title = "Shout Failure", color = discord.Color.dark_red())
+        embed_var.set_author(name = username_to_use, icon_url = str(message.author.avatar_url))
+        embed_var.add_field(
+            name = "Failed to shout message from " + message.channel.name + ".",
+            value = formatted_shout_message
+        )
+        embed_var.set_footer(text="Please report this to SimplestUsername.")
+        
+        log_channel = client.get_channel(settings["discord_log_channel_id"])
+        await log_channel.send(embed=embed_var)
+        raise
+    else:
+        embed_var = discord.Embed(title = "Shout Succeeded", color = discord.Color.dark_red())
+        embed_var.set_author(name = username_to_use, icon_url = str(message.author.avatar_url))
+        embed_var.add_field(
+            name = "Shouted message from " + message.channel.name + " successfully.",
+            value = formatted_shout_message
+        )
+        embed_var.set_footer(text="Any issues? Contact SimplestUsername. Elapsed time: " + str(time.perf_counter() - performance_timer_start))
+        
+        log_channel = client.get_channel(settings["discord_log_channel_id"])
+        await log_channel.send(embed=embed_var)
+
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author.bot: 
+            return
+
+        if message.channel.id == settings["discord_notification_channel_id"]:
+            await format_and_shout(self, message)
+
+discord_client = DiscordClient()
+discord_client.run(credentials["discord_token"])
